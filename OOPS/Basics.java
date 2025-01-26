caa098c03e6fa669f80daeee7de271f7e2a60d65
neerajdeshmukh11 <godnvd999@gmail.com>
2025-01-26 09:57:55 +0530

    oops basics

---
+++ OOPS/Basics.java
@@ -0,0 +1,41 @@
+public class Basics {
+    public static void main(String[] args) {
+        Pen p1 = new Pen(); // p1 object
+        p1.setColor("blue");
+        System.out.println(p1.color);
+        p1.setTip(5);
+        System.out.println(p1.tip);
+        p1.color = "yellow";
+        System.out.println(p1.color);
+
+        // using getter and setter
+
+        p1.setColor("blue");
+        System.out.println(p1.getColor());
+        p1.setTip(5);
+        System.out.println(p1.getTip());
+        p1.setColor("yellow");
+        System.out.println(p1.getColor());
+    }
+
+}
+// class pen
+class Pen {
+    String color;
+    int tip;
+
+    String getColor(){
+       return this.color; // this keyword refers to the current object
+    }
+
+    int getTip(){
+        return this.tip;
+    }
+
+    void setColor(String color){
+        this.color = color;
+    }
+    void setTip(int tip){
+        this.tip = tip;
+    }
+} 
